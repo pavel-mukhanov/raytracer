9f4183417cb74f4af7fc1d5d7f73bf815a83991d
pavel-mukhanov <mukhanovpv@gmail.com>
2018-02-26 22:55:19 +0300

    Add Vector struct

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
-//! An example of generating julia fractals.
-extern crate num_complex;
 extern crate image;
+extern crate num_complex;
 extern crate rand;
 
 use std::fs::File;
 use image::{ImageBuffer, Rgb};
+use rand::Rng;
+use rand::distributions::{IndependentSample, Normal};
 
 use num_complex::Complex;
 
 fn render(pixels: &mut Vec<Rgb<u8>>) {
     let max_iterations = 256u16;
+    let between = Normal::new(1.0, 1.);
     let mut rng = rand::thread_rng();
 
-    for pixel in pixels.iter_mut() {
-        *pixel = image::Rgb([0, 255, 0]);
+    for (i, pixel) in pixels.iter_mut().enumerate() {
+        let b = between.ind_sample(&mut rng).abs();
+        *pixel = image::Rgb([0, 127 + (b * 10f64) as u8, (i % 255) as u8]);
     }
 }
 
-fn write_image(pixels:Vec<Rgb<u8>>, imgx:u32, imgy:u32) {
+fn write_image(pixels: Vec<Rgb<u8>>, imgx: u32, imgy: u32) {
     let mut imgbuf = ImageBuffer::new(imgx as u32, imgy as u32);
- 
+
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         let index = x * imgx as u32 + y;
         *pixel = pixels[index as usize];
@@ -30,8 +33,8 @@ fn write_image(pixels:Vec<Rgb<u8>>, imgx:u32, imgy:u32) {
 }
 
 fn main() {
-    let imgx = 4;
-    let imgy = 4;
+    let imgx = 400;
+    let imgy = 400;
     let mut pixels = vec![Rgb([0, 0, 0]); imgx * imgy];
 
     render(&mut pixels);
